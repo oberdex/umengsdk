6e9d86e5f76b29680d78c31aea4c01ef10c12183
Jacky <jacky@5amax.com>
2019-11-04 15:35:25 +0800

    f

--- src/Dispatch.php
+++ src/Dispatch.php
@@ -7,30 +7,30 @@ namespace oberdex\umengsdk;
 use Hanson\Foundation\Foundation;
 
 /**
- * Class SDK
+ * Class Dispatch
  * @package oberdex\youmengsdk
  * @Method getAppList 获取所有app
  */
 class Dispatch extends Foundation
 {
-    private $umeng;
+    private $umengsdk;
 
     /**
-     * SDK constructor.
-     * @param $sdk
+     * Dispatch constructor.
+     * @param $config
      */
     public function __construct($config)
     {
         parent::__construct($config);
         if (isset($config['page']) && isset($config['perPage'])) {
-            $this->umeng = new Umeng($config['apiKey'], $config['apiSecurity'], $config['page'], $config['perPage']);
+            $this->umengsdk = new Umeng($config['apiKey'], $config['apiSecurity'], $config['page'], $config['perPage']);
         } else {
-            $this->umeng = new Umeng($config['apiKey'], $config['apiSecurity']);
+            $this->umengsdk = new Umeng($config['apiKey'], $config['apiSecurity']);
         }
     }
 
     public function __call($name, $arguments)
     {
-        $this->umeng->{$name}(...$arguments);
+        $this->umengsdk->{$name}(...$arguments);
     }
 }
